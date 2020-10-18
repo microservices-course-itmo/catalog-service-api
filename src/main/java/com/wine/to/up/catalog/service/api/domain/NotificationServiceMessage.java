2a213eeec9e1042128d3da9c36250ecb7c6770dd
AlexKolc <alex_kolc@inbox.ru>
2020-10-18 23:21:58 +0300

    For messaging to notification-service

---
+++ src/main/java/com/wine/to/up/catalog/service/api/domain/NotificationServiceMessage.java
@@ -0,0 +1,18 @@
+package com.wine.to.up.catalog.service.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class NotificationServiceMessage {
+    private String id;
+
+    private String name;
+
+    private double price;
+}
