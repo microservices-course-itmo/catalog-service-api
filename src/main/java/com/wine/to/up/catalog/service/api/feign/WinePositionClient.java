2abb910e4603da17362023f2ecffdf6e4f91b636
santatostada <asabinayulia@gmail.com>
2021-03-07 21:41:01 +0300

    add annotations

--- src/main/java/com/wine/to/up/catalog/service/api/feign/WinePositionClient.java
+++ src/main/java/com/wine/to/up/catalog/service/api/feign/WinePositionClient.java
@@ -2,6 +2,7 @@ package com.wine.to.up.catalog.service.api.feign;
 
 import com.wine.to.up.catalog.service.api.dto.WinePositionTrueResponse;
 import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 public interface WinePositionClient {
     @RequestLine(value = "GET /position/true/trueSettings")
     @Headers("Content-Type: application/json")
-    public List<WinePositionTrueResponse> getAllWinePositionsTrue(String page, String amount, List<String> sortByPair, String filterBy);
+    public List<WinePositionTrueResponse> getAllWinePositionsTrue(@Param("page") String page,
+                                                                  @Param("amount") String amount,
+                                                                  @Param("sortByPair") List<String> sortByPair,
+                                                                  @Param("filterBy") String filterBy);
 }
