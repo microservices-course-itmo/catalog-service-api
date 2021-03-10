a4b63455f847f16b0a57600f3b8ef15edffa9eeb
santatostada <asabinayulia@gmail.com>
2021-03-10 12:40:50 +0300

    fix mapping

--- src/main/java/com/wine/to/up/catalog/service/api/feign/FavoriteWinePositionsClient.java
+++ src/main/java/com/wine/to/up/catalog/service/api/feign/FavoriteWinePositionsClient.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface FavoriteWinePositionsClient extends FavoriteWinePositionsService {
-    @RequestLine(value = "GET /position/true/favorites/")
+    @RequestLine(value = "GET /position/true/favourites/")
     @Headers("Content-Type: application/json")
-    List<WinePositionTrueResponse> getFavourites(@QueryMap Map<String, List<String>> favouritePosition);
+    List<WinePositionTrueResponse> getFavourites(@QueryMap Map<String, List<String>> favoritePosition);
 }
