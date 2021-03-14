c131b62059984483659ccb74e6c0b2e97252ecee
Aleksandra Dedovich <amdedovich@yandex.ru>
2021-03-14 20:31:27 +0300

    add feign for wine positions

--- src/main/java/com/wine/to/up/catalog/service/api/feign/WinePositionClient.java
+++ src/main/java/com/wine/to/up/catalog/service/api/feign/WinePositionClient.java
@@ -3,15 +3,17 @@ package com.wine.to.up.catalog.service.api.feign;
 import com.wine.to.up.catalog.service.api.dto.WinePositionTrueResponse;
 import feign.Headers;
 import feign.Param;
+import feign.QueryMap;
 import feign.RequestLine;
 
 import java.util.List;
+import java.util.Map;
 
 public interface WinePositionClient {
     @RequestLine(value = "GET /position/true/trueSettings")
     @Headers("Content-Type: application/json")
-    public List<WinePositionTrueResponse> getAllWinePositionsTrue(@Param("page") String page,
-                                                                  @Param("amount") String amount,
-                                                                  @Param("sortByPair") List<String> sortByPair,
-                                                                  @Param("filterBy") String filterBy);
+    List<WinePositionTrueResponse> getAllWinePositionsTrue(@Param("page") String page,
+                                                          @Param("amount") String amount,
+                                                          @QueryMap Map<String, List<String>> sortByPair,
+                                                          @Param("filterBy") String filterBy);
 }
